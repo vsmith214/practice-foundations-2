73f04edf4fce9cd7e7043208b0dadbc2bb722739
Scott D'Alessandro <sdaless22@gmail.com>
2016-08-29 10:57:04 -0400

    Update reject_spec.js

--- 02_reject/reject_spec.js
+++ 02_reject/reject_spec.js
@@ -2,7 +2,7 @@ describe("reject function", function() {
   
   window.isNegNum; // attaches the function isNegNum to the global window object	
   
-  it("Returns the values in the array that return 'false' after the function", function() {
+  it("Returns the values in the array that return 'false' after the function argument is invoked", function() {
     var arrayOfNumbers = [4, -57, 1778, -43, 9, 89, -345];
     isNegNum = function(num) { return num < 0 };	
 
